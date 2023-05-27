127203caf3e18ec960bf7caa466360988717ed89
Gustavo Engers <gustavoengers6441@gmail.com>
2023-05-26 23:58:49 -0300

    Boilerplate backend

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [PokemonModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
