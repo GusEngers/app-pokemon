297f4716797becc1e8ad8a8a6db656e276271fe8
Gustavo Engers <gustavoengers6441@gmail.com>
2023-06-07 18:41:45 -0300

    Filtro por generación y por tipo

--- api/src/filter/filter.module.ts
+++ api/src/filter/filter.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { FilterService } from './filter.service';
 import { FilterController } from './filter.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  PokemonList,
+  PokemonListSchema,
+} from 'src/schemas/pokemon_list.schema';
+import { Generation, GenerationSchema } from 'src/schemas/generation.schema';
+import { Type, TypeSchema } from 'src/schemas/type.schema';
 
 @Module({
   controllers: [FilterController],
-  providers: [FilterService]
+  providers: [FilterService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: PokemonList.name, schema: PokemonListSchema },
+      { name: Generation.name, schema: GenerationSchema },
+      { name: Type.name, schema: TypeSchema },
+    ]),
+  ],
 })
 export class FilterModule {}
--- api/src/filter/filter.service.ts
+++ api/src/filter/filter.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Generation } from 'src/schemas/generation.schema';
+import { PokemonList } from 'src/schemas/pokemon_list.schema';
+import { Type } from 'src/schemas/type.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class FilterService {}
+export class FilterService {
+  constructor(
+    @InjectModel(Generation.name) private _Generation: Model<Generation>,
+    @InjectModel(Type.name) private _Type: Model<Type>,
+    @InjectModel(PokemonList.name) private _PokemonList: Model<PokemonList>,
+  ) {}
+
+  async pokemonsGeneration(generation: string, page: number = 1) {
+    try {
+      const pokemons = await this._PokemonList
+        .find({ generation })
+        .sort({ _id: 1 })
+        .skip((page - 1) * 20)
+        .limit(20)
+        .select('-__v')
+        .populate([
+          { path: 'types', select: '-__v' },
+          { path: 'generation', select: '-__v' },
+        ]);
+      const count = await this._PokemonList.count({ generation });
+      return { count, pokemons };
+    } catch (error: any) {
+      throw new HttpException(error.message, error.statusCode);
+    }
+  }
+
+  async pokemonsTypes(type: string, page: number = 1) {
+    try {
+      const pokemons = await this._PokemonList
+        .find({ types: type })
+        .sort({ _id: 1 })
+        .skip((page - 1) * 20)
+        .limit(20)
+        .select('-__v')
+        .populate([
+          { path: 'types', select: '-__v' },
+          { path: 'generation', select: '-__v' },
+        ]);
+      const count = await this._PokemonList.count({ types: type });
+      return { count, pokemons };
+    } catch (error: any) {
+      throw new HttpException(error.message, error.statusCode)
+    }
+  }
+}
